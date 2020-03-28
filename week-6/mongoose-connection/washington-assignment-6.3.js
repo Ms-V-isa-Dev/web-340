be024bbcad7f76856c999cb2732becf4dfe6a5cb
Verlee Washington <VWashington@my365.bellevue.edu>
2020-03-27 22:12:29 -0500

    Adding assignment 6.3

--- week-6/mongoose-connection/washington-assignment-6.3.js
+++ week-6/mongoose-connection/washington-assignment-6.3.js
@@ -1,3 +1,16 @@
+/*
+============================================
+; Title:  Exercise 6.3
+; Author: Professor Krasso
+; Date:   27 March 2020
+; Modified by: Verlee Washington
+; Description: Creating and using Mongoose and
+; MongoDB
+;===========================================
+*/
+
+// Required header no longer registering
+//var header=require('..\Washington-header.js');
 
 // Require statements
 var express=require('express');
@@ -5,6 +18,7 @@ var mongoose=require('mongoose');
 var http=require('http');
 var logger=require('morgan');
 
+// Specify our database connection and pass in the connection string
 var mongoDB='mongodb+srv://admin:Isatest@buwebdev-cluster-1-qc6an.mongodb.net/test';
 mongoose.connect(mongoDB, {
   useMongoClient: true
@@ -12,17 +26,23 @@ mongoose.connect(mongoDB, {
 
 mongoose.Promise=global.Promise;
 
+// Database variable
 var db=mongoose.connection;
 
+// General error handling
 db.on("error", console.error.bind(console, "MongoDB connected error: "));
 
+// Output
 db.once("open", function(){
   console.log("Application connected to mLab MongoDB instance");
 });
 
+// Creat our express server
 var app=express();
 app.use(logger('dev'));
 
+
+// Create http server and pass in an app
 http.createServer(app).listen(3000, function(){
   console.log('Application started and listening on port 3000');
 })
